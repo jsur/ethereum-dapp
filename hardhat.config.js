7d839f03b06b31ec5c74ab36d2f4a1a44d53f39d
julius.suominen <julius.suominen@gmail.com>
2021-05-15 22:26:08 +0300

    add hardhat configs, 1st contract interaction

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+require("@nomiclabs/hardhat-waffle");
+
+dotenv.config();
+
+console.log('process.env.ROPSTEN_PRIVATE_KEY:', process.env.ROPSTEN_PRIVATE_KEY);
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/06337af0e0bd4467acf96630197484ae",
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
+};
